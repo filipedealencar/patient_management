a9a5c47cf4dc33269b07378ebaa024967efc458b
filipedealencar <luis.f.alencar@gmail.com>
2023-12-21 16:24:26 -0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,24 +3,12 @@ import { AppProps } from "next/app";
 import GlobalStyle from "@/styles/GlobalStyles";
 import theme from "@/styles/theme";
 import React, { ReactElement, ReactNode, useEffect, useState } from "react";
-import { NextPage } from "next";
 import "@/styles/fonts.css";
 import { GlobalContextProvider } from "@/contexts/GlobalContext";
 import SidebarWithHeader from "@/layouts/SidebarWithHeader";
 import { Toaster } from "react-hot-toast";
 
-export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-const App: React.FC<AppProps> = ({
-  Component,
-  pageProps,
-}: AppPropsWithLayout) => {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
